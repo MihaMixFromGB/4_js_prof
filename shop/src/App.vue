47b5e044b89c4306df3f946275abf4c6a871e6be
MihaMixFromGB <mihamix@yandex.ru>
2022-02-15 22:02:06 +0700

    Lesson 6

---
+++ shop/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="page">
+    <header>
+      <div class="cart">cart</div>
+    </header>
+    <main>
+      <card v-for="(product, i) in products" :key="i" :data="product"></card>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+  import card from './components/card';
+
+  export default {
+    components: { card },
+    data() {
+      return {
+        products: [
+          {
+            name: 't-shirt-1',
+            desc: 'desc of t-shirt-1',
+            price: 10
+          },
+          {
+            name: 't-shirt-2',
+            desc: 'desc of t-shirt-2',
+            price: 20
+          },
+          {
+            name: 't-shirt-3',
+            desc: 'desc of t-shirt-3',
+            price: 30
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .page {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: min-content 1fr min-content;
+  }
+  header {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    min-height: 75px;
+    background-color: grey;
+    & > .cart {
+      height: 25px;
+      width: 100px;
+      padding: 10px;
+      margin-right: 10px;
+      text-align: center;
+      background-color: white;
+    }
+  }
+  main {
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+  }
+  footer {
+    min-height: 75px;
+    background-color: grey;
+  }
+</style>
---
+++ shop/src/components/card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        <h2>{{data.name}}</h2>
+        <p>{{data.desc}}</p>
+        <p>${{data.price}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'data'
+    ]
+}
+</script>
+
+<style lang="scss">
+    .card {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100px;
+        margin: 10px;
+        padding: 10px;
+        border: 1px solid grey;
+        border-radius: 10%;
+    }
+</style>
